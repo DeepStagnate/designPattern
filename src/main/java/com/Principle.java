c9f93319e737192822b20308693df298201a7ddd
crimson <causal_content@163.com>
2019-05-04 23:32:47 +0800

    初始化

---
+++ src/main/java/com/Principle.java
@@ -0,0 +1,19 @@
+package com;
+
+/**
+ * @author Administrator
+ * @Title: Principle
+ * @ProjectName designPattern
+ * @Description: TODO 设计原则
+ * 单一原则
+ * 依赖倒置
+ * 接口隔离
+ * 里氏替换
+ * 迪米特原则（高内聚，低耦合）
+ * 开闭原则
+ * @date 2019/5/4 000423:03
+ */
+public class Principle {
+
+
+}
